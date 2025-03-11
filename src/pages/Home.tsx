0f447031bfd1ae00e94cc52bd934d0d832c9156f
Esther Reis <estherjuliane_reis@hotmail.com>
2025-03-11 15:52:21 +0100

    Stashed changes pulled to main-page branch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <p>YOU FIT</p>
+        <button>about</button>
+      </header>
+      <main>
+        <img
+          src="https://images.unsplash.com/photo-1571019613576-2b22c76fd955?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Woman Training"
+        />
+        <h1>FIND YOUR PERFECT WORKOUT</h1>
+        <button>View Exercises</button>
+        <button>Build Your Workout</button>
+      </main>
+      <footer>
+        <button>WORKOUT FOR BEGINNERS</button>
+        <button> IMPROVE YOUR ENDURANCE</button>
+        <button>15 MINUTES WORKOUTS</button>
+        <button>CARDIO</button>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
