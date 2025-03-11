5079bb3fe50d8d7ac37b4d4a5df17243d8aa7dd5
Esther Reis <estherjuliane_reis@hotmail.com>
2025-03-11 17:27:24 +0100

    Installed css modules in the project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import styles from "../pages/Home.module.css";
 
 const Home = () => {
   return (
     <div>
       <header>
-        <p>YOU FIT</p>
+        <h2>YOU FIT</h2>
         <button>about</button>
       </header>
+
       <main>
         <img
+          className={styles.image}
           src="https://images.unsplash.com/photo-1571019613576-2b22c76fd955?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Woman Training"
         />
@@ -16,6 +18,7 @@ const Home = () => {
         <button>View Exercises</button>
         <button>Build Your Workout</button>
       </main>
+
       <footer>
         <button>WORKOUT FOR BEGINNERS</button>
         <button> IMPROVE YOUR ENDURANCE</button>
